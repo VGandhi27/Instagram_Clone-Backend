f091616e34d26544651aee13679325efd8294184
Vidushi Gandhi <curiousvidushi27@gmail.com>
2023-09-02 22:06:21 +0530

    backend part 4

--- App.js
+++ App.js
@@ -3,7 +3,7 @@ const express = require('express')
 const app=express()
 const mongoose =require('mongoose')
 const PORT =process.env.PORT || 4000
-const {MONGOURI}=require("./Key")
+const {MONGOURI}=require("./prod")
 
 mongoose.connect(MONGOURI,{
     useNewUrlParser:true,
